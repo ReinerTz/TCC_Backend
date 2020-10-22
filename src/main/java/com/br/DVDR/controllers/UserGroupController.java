499a325df0990bed1f16d98bedd9d7a6ad87cdf1
ReinerTz <llreinert@gmail.com>
2020-10-21 23:24:33 -0300

    end point para pesquisar usuários que estão em um determinado grupo

--- src/main/java/com/br/DVDR/controllers/UserGroupController.java
+++ src/main/java/com/br/DVDR/controllers/UserGroupController.java
@@ -1,11 +1,15 @@
 package com.br.DVDR.controllers;
 
 import com.br.DVDR.models.UserGroupModel;
+import com.br.DVDR.models.UserModel;
 import com.br.DVDR.repository.UserGroupRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "api")
@@ -20,4 +24,17 @@ public class UserGroupController {
     public UserGroupModel save(@RequestBody UserGroupModel user){
         return userGroupRepository.save(user);
     }
+
+    @GetMapping("/usergroups")
+    @ApiOperation(value = "Retorna todos os usuários")
+    public List<UserGroupModel> allUsers(){
+        return userGroupRepository.findAll();
+    }
+
+    @GetMapping("/usergroup/group/{id}")
+    @ApiOperation(value = "Retorna todos os usuários que pertencem a um determinado grupo")
+    public Optional<List<UserGroupModel>> findUser(@PathVariable(value = "id") Long id){
+        return userGroupRepository.findUsersByGroup(id);
+    }
+
 }
--- src/main/java/com/br/DVDR/repository/UserGroupRepository.java
+++ src/main/java/com/br/DVDR/repository/UserGroupRepository.java
@@ -1,7 +1,15 @@
 package com.br.DVDR.repository;
 
+import com.br.DVDR.models.GroupModel;
 import com.br.DVDR.models.UserGroupModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserGroupRepository extends JpaRepository<UserGroupModel, Long> {
+    @Query(value = "SELECT u.* FROM tb_usergroup u WHERE u.group_id = :group", nativeQuery = true)
+    Optional<List<UserGroupModel>> findUsersByGroup(@Param("group")Long group);
 }
